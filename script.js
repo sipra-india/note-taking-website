2745b9a4bc102b627487bd6615f6b6f202e140c5
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-06-03 13:25:39 +0530

    problrm number 1

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function Add_note(){
-    const name = document.querySelector('.add_note')
-    console.log(name);
+    const place = document.querySelector('.nav_bar');
+    const name = document.querySelector('.add_note').value;
+    const div = document.createElement('div');
+    const li = document.createElement('li');
+    const btn1 = document.createElement('button');
+    const img1 = document.createElement('img');
+    const btn2 = document.createElement('button');
+    const img2 = document.createElement('img');
+    const btn3 = document.createElement('button');
+    const img3 = document.createElement('img');
+
+    div.setAttribute('id',name);
+    div.setAttribute('class','nav_item');
+    btn1.setAttribute('class','btn');
+    btn2.setAttribute('class','btn');
+    btn3.setAttribute('class','btn');
+    img1.setAttribute('class','btn_icon');
+    img1.src = "C:\Users\Subrat Sir\Downloads\icons8-edit-50.png";
+    img2.setAttribute('class','btn_icon');
+    img2.src = "C:\Users\Subrat Sir\Downloads\icons8-download-24.png";
+    img3.setAttribute('class','btn_icon');
+    img3.src="C:\Users\Subrat Sir\Downloads\icons8-delete-30.png";
+    li.textContent = name;
+    div.appendChild(li);
+    div.appendChild(btn1);
+    div.appendChild(btn2);
+    div.appendChild(btn3);
+    btn1.appendChild(img1);
+    btn2.appendChild(img2);
+    btn3.appendChild(img3);
+    place.appendChild(div);
 }
 function save(){
 
