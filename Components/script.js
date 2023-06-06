37eb347f9f703a330891d66041357d887ceca6c0
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-06-07 00:01:52 +0530

    done foe the day

--- Components/script.js
+++ Components/script.js
@@ -1,61 +1,64 @@
 let id = 0;
 
-    function counter(){
-        id = id + 1;
+function counter(){
+    id = id + 1;
+}
+
+function Add_note(){
+    const note = document.querySelector('.add_note').value;
+    const name = note.trim();
+    if (name === ""){
+        alert('please enter a name for your note');
+    } else {
+        const place = document.querySelector('.nav_bar');
+        const div = document.createElement('div');
+        const btndiv = document.createElement('div');
+        const li = document.createElement('li');
+        const btn1 = document.createElement('button');
+        const img1 = document.createElement('img');
+        const btn2 = document.createElement('button');
+        const img2 = document.createElement('img');
+        const btn3 = document.createElement('button');
+        const img3 = document.createElement('img');
+
+        const idn = id;
+
+        div.setAttribute('id', idn);
+        btndiv.setAttribute('class', btndiv);
+        div.setAttribute('class','nav_item');
+        btn1.setAttribute('class','btn');
+        btn2.setAttribute('class','btn');
+        btn3.setAttribute('class','btn');
+        img1.setAttribute('class','btn_icon');
+        img1.src = "icons8-edit-50.png";
+        img2.setAttribute('class','btn_icon');
+        img2.src = "icons8-download-24.png";
+        img3.setAttribute('class','btn_icon');
+        img3.src="icons8-delete-30.png";
+        img3.onclick = function (){
+            delete_item(idn)
+        };
+        li.textContent = name;
+        li.setAttribute('class','li_item')
+        div.appendChild(li);
+        btndiv.appendChild(btn1);
+        btndiv.appendChild(btn2);
+        btndiv.appendChild(btn3);
+        btn1.appendChild(img1);
+        btn2.appendChild(img2);
+        btn3.appendChild(img3);
+        div.appendChild(btndiv);
+        place.appendChild(div);
+        console.log(div);
     }
+}
 
-    function Add_note(){
-        const note = document.querySelector('.add_note').value;
-        const name = note.trim();
-        if (name === ""){
-            alert('please enter a name for your note');
-        } else {
-            const place = document.querySelector('.nav_bar');
-            const div = document.createElement('div');
-            const li = document.createElement('li');
-            const btn1 = document.createElement('button');
-            const img1 = document.createElement('img');
-            const btn2 = document.createElement('button');
-            const img2 = document.createElement('img');
-            const btn3 = document.createElement('button');
-            const img3 = document.createElement('img');
+function clear_input(){
+    const input = document.querySelector('.add_note');
+    input.value = "";
+}
 
-            const idn = id;
-
-            div.setAttribute('id', idn);
-            div.setAttribute('class','nav_item');
-            btn1.setAttribute('class','btn');
-            btn2.setAttribute('class','btn');
-            btn3.setAttribute('class','btn');
-            img1.setAttribute('class','btn_icon');
-            img1.src = "icons8-edit-50.png";
-            img2.setAttribute('class','btn_icon');
-            img2.src = "icons8-download-24.png";
-            img3.setAttribute('class','btn_icon');
-            img3.src="icons8-delete-30.png";
-            img3.onclick = function (){
-                delete_item(idn)
-            };
-            li.textContent = name;
-            li.setAttribute('class','li_item')
-            div.appendChild(li);
-            div.appendChild(btn1);
-            div.appendChild(btn2);
-            div.appendChild(btn3);
-            btn1.appendChild(img1);
-            btn2.appendChild(img2);
-            btn3.appendChild(img3);
-            place.appendChild(div);
-            console.log(div);
-        }
-    }
-
-    function clear_input(){
-        const input = document.querySelector('.add_note');
-        input.value = "";
-    }
-
-    function create(){
+function create(){
         counter();
 
         Add_note();
